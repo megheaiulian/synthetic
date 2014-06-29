e99584d256c955aa91259fde6ccb3a8f2cc0630b
Iulian Meghea <iulian.meghea@gmail.com>
2014-06-29 16:31:13 +0300

    Added request with promise

---
+++ src/synthetic/request.js
@@ -0,0 +1,51 @@
+var get = "GET";
+var request = function(url,method,data){
+	method = method || get;
+	data 	= data || {};
+	
+	if(method instanceof Object){
+		data 	= method;
+		method	= get;
+	}
+
+	var req = (function() {
+		if (typeof 'XMLHttpRequest' !== 'undefined') {
+			// CORS (IE8-9)
+			return url.indexOf('http') === 0 && typeof XDomainRequest !== 'undefined'?
+				new XDomainRequest(): new XMLHttpRequest();
+		} else if (typeof 'ActiveXObject' !== 'undefined') {
+			return new ActiveXObject('Microsoft.XMLHTTP');
+		}
+	})();
+	if (!req) {
+		throw new Error ('Browser doesn\'t support XHR');
+	}
+	// serialize data?
+	if (typeof data !== 'string') {
+		var serialized = [];
+		for (var datum in data) {
+			serialized.push(datum + '=' + data[datum]);
+		}
+		data = serialized.join('&');
+	}
+	return new Promise(function(resolve,reject){
+		req.onload = function(){
+			if(req.status == 200){
+				resolve(req.response);
+			}else{
+				reject(Error(req.statusText));
+			}
+		};
+		req.onerror = function(){
+			reject(Error('Network Error'));
+		};
+
+		//open connection
+		req.open(method, (method === 'GET' && data ? url+'?'+data : url), true);
+		
+		// send it
+		req.send(method !== 'GET' ? data : null);
+	})
+}
+
+module.exports = request;
